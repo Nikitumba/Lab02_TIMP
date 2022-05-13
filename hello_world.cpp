67849be91c21dc1b4eb4adf167a5c530f9667cb4
Nikitumba <nikita.ivvanov@mail.ru>
2022-05-13 19:35:29 +0300

    comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,6 +3,6 @@
 int main(){
 std::cout << "Введите ваше имя"<< std::endl;
 std::string name;
-std::cin >> name;
-std::cout <<"Hello world from " << name;
+std::cin >> name;// enter name
+std::cout <<"Hello world from " << name;// print the name
 }
