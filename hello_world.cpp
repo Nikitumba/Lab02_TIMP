3552a08883ece3e57c0caf46e2547dcf88d64de2
Nikitumba <nikita.ivvanov@mail.ru>
2022-05-20 17:13:55 +0300

    hello_world

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int
 main()
 {
-  cout << "Введите ваше имя";
+  cout << "Введите ваше имя";//i wanna sleep 
   string name;
   cin >> name;
   cout << "Hello world from " << name;
