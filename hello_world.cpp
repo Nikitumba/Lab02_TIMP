91e23e54f8c1f2eb8b562bde6f3dc5774654ec03
Nikitumba <nikita.ivvanov@mail.ru>
2022-06-07 17:57:54 +0300

    eddedededededede

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int
 main()
 {
-  cout << "Введите ваше имя";//i wanna gerantus 
+  cout << "Введите ваше имя";//i wanna be slave 
   string name;
   cin >> name;
   cout << "Hello world from " << name;
